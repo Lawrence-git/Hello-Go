8f793a321c4bc7488206c74f63210040fb9a8bf1
RuzzZZ <xzy199400@foxmail.com>
2017-06-22 14:31:27 +0800

    switch demo
    Signed-off-by: RuzzZZ <xzy199400@foxmail.com>

--- chapter3/switch_demo.go
+++ chapter3/switch_demo.go
@@ -4,6 +4,27 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("Hello World")
+func switchDemo() {
+	switchInt(1)
+	switchInt(2)
+	switchInt(3)
+	switchInt(4)
+	switchInt(5)
+	switchInt(6)
+}
+
+func switchInt(aInt int) {
+	switch aInt {
+	case 0:
+		fmt.Println("000000")
+	case 1:
+		fmt.Println("1111111")
+	case 2:
+	case 3:
+		fmt.Println("33333333")
+	case 4, 5:
+		fmt.Println("444455555")
+	default:
+		fmt.Println("Default")
+	}
 }
