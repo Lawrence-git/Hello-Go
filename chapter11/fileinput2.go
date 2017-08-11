09acd73b106931fb3e1a6499476894b8647e98f1
RuzzZZ <xzy199400@foxmail.com>
2017-08-11 17:50:05 +0800

    bugfix

--- chapter11/fileinput2.go
+++ chapter11/fileinput2.go
@@ -17,6 +17,6 @@ func fileinput2() {
 	fmt.Printf("%s\n", string(buf))
 	err2 := ioutil.WriteFile(outputFile, buf, 0644)
 	if err2 != nil {
-		fmt.Fprintf(os.Stderr, "Write File Filed: %s\n", err1)
+		fmt.Fprintf(os.Stderr, "Write File Filed: %s\n", err2)
 	}
 }
